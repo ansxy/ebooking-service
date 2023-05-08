e9e9db345a983e32c3f4c0cb30f654336a02394d
ansxy <ansarfadillah17@gmail.com>
2023-05-08 14:30:29 +0800

    TODO : add middleware on every req

--- hotels-service/src/routes/hotels.route.js
+++ hotels-service/src/routes/hotels.route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 
 const { createHotel, getHotelById } = require('../controller/hotels.controller')
+const verifyToken = require('../lib/verifyToken')
 //TODO : Add Route For new API
 
-router.post('/create', createHotel)
+router.post('/create',verifyToken, createHotel)
 router.get('/hotel/:hotelId', getHotelById) 
 
 module.exports = router
--- hotels-service/src/routes/rooms.route.js
+++ hotels-service/src/routes/rooms.route.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 
 const { getAllRoom, createRoom, getRoomById } = require('../controller/rooms.controller')
 //TODO : Add Route For new APi
+//TODO : Taro di middlewarenya ini : verifyToken // Diimport dulu c
+//CONTOH :  router.post('/create',verifyToken, createHotel)
+ 
 
 router.get('/', getAllRoom)
 router.post('/room/create', createRoom)
