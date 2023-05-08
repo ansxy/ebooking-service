a6c7b0697c5e5a0c71905f1ec8f6faf1ed15a74c
ansxy <ansarfadillah17@gmail.com>
2023-05-08 14:28:48 +0800

    jwtverify middleware

--- hotels-service/src/lib/verifyToken.js
+++ hotels-service/src/lib/verifyToken.js
@@ -1,10 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-async function verifyToken(req) {
+async function verifyToken(req,res,next) {
     const authHeader = req.headers.authorization
-    const token = authHeader.split(' ')[1]
-    const verify = jwt.verify(token, process.env.JWT_SECRET)
-    return verify
+    if(authHeader) {
+        const token = authHeader.split(' ')[1]
+        jwt.verify(token, process.env.JWT_SECRET,(err,user) => {
+            if(err){
+                return res.sendStatus(403)
+            }
+            req.user = user
+            next()
+        })
+    } else {
+        res.sendStatus(401)
+    }
 }
 
 module.exports = verifyToken
